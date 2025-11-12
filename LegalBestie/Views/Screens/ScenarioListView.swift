d8a0a8ab45b642a6177bf36d51cbfb3b6a502e46
Carolina Lupanciuc <103419236+karolc222@users.noreply.github.com>
2025-11-12 16:30:02 +0100

    ScenarioListView work on functions

--- LegalBestie/Views/Screens/ScenarioListView.swift
+++ LegalBestie/Views/Screens/ScenarioListView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 private struct ScenarioHeader: Decodable {
     let scenarioId: String
     let scenarioTitle: String
-    let scenarioName: String
     let scenarioDescription: String
     let scnearioUpdatedAt: String
 }
@@ -18,17 +17,39 @@ private struct ScenarioHeader: Decodable {
 //one row in the list
 private struct ScenarioListItem: Identifiable {
     let scenarioId = UUID()
-    let scenarioName : String
-    let scenarioTitle: String
-    let scenarioDescription: String
-    let scenarioUpdatedAt: Date
+    let fileName : String
+    let fileTitle: String
+    let fileDescription: String
+    let fileUpdatedAt: Date
 }
 
 struct ScenarioListView: View {
     let category: String
+    @State private var items: [ScenarioListItem] = []
+    @State private var errorText: String?
     
     
     var body: some View {
-        Text("Scenario list will apear here ")
+        List(items) { item in
+            NavigationLink(destination: Text(item.title)) {
+                VStack(alignement: .leading) {
+                    Text(item.title).font(.headline)
+                    Text(item.description).font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+            }
+        }
+        .navigationTitle(category)
+        .task { await load() }
+    }
+    private func load() async {
+        do {
+            guard let urls = Bundle.main.urls(forResourcesWithExtension: "json", subdirectory: "JSON/\(category)") else {
+                return
+            }
+            let decoder = JSON decoder()
+            let df = DateFormatter()
+            
+        }
     }
 }
